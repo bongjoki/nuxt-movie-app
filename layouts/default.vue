45a200f2ffd8e323b3210b42cb7e6abbb9fb6f51
Bongjo Ki <bongjo@starshell.co>
2021-04-20 15:47:25 +0900

    Initial Commit

---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app dark>
+    <v-system-bar app>
+      <v-spacer></v-spacer>
+
+      <v-icon>mdi-square</v-icon>
+
+      <v-icon>mdi-circle</v-icon>
+
+      <v-icon>mdi-triangle</v-icon>
+    </v-system-bar>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" fixed temporary>
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({ drawer: null }),
+}
+</script>
